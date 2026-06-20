2d4d36bc9a3a6e92a4aad4aa2154df7f55e3f229
nothingblooms <nothingblooms@gmail.com>
2026-06-20 17:38:28 -0500

    Update Lgbtics.kt

--- src/en/lgbtics/src/eu/kanade/tachiyomi/extension/en/lgbtics/Lgbtics.kt
+++ src/en/lgbtics/src/eu/kanade/tachiyomi/extension/en/lgbtics/Lgbtics.kt
@@ -4,13 +4,14 @@ import eu.kanade.tachiyomi.multisource.madara.Madara
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class Lgbtics : Madara(
-    "Lgbtics",
-    "https://lgbtics.com",
-    "en"
-) {
+class Lgbtics :
+    Madara(
+        "Lgbtics",
+        "https://lgbtics.com",
+        "en",
+    ) {
     override val dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.US)
-    
+
     // Overrides the default "/manga/" path to match the site's custom structure
     override val mangaUrlDirectory = "comic"
 }
